b055e6a6dad3a728d1c5451b34a80b5911b43f36
Elcan Seyidov <elcanseyidov1223@gmail.com>
2021-04-01 16:34:09 +0400

    1

---
+++ Week3Task/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Week3Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, d;
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            c = Convert.ToInt32(Console.ReadLine());
+            d = (a + b + c) / 3;
+            var netice = d >= 45 ? "Beli" : "Xeyr";
+            Console.WriteLine(netice);
+        }
+    }
+}
