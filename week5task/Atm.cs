ece1de4eb8571f4898f371236bf6a6a71a68de47
Elcan Seyidov <elcanseyidov1223@gmail.com>
2021-04-15 11:25:44 +0400

    Week 5 task 2 update

---
+++ week5task/Atm.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace week5task
+{
+
+    class Atm
+    {
+        public static int pul = 1000;
+        public static Hashtable cashdate = new Hashtable();
+        public static bool Login(int pin)
+        {
+            int log = 0;
+            int[] pinArray = new int[] { 2312, 7665, 8712, 1923, 9987 };
+            foreach (int item in pinArray)
+            {
+                if (item == pin) { log++; }
+            }
+            if (log > 0) return true;
+            else return false;
+        }
+
+        public static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("MENU");
+            Console.WriteLine("Negd pulun verilmesi üçün: 1");
+            Console.WriteLine("Balansı yoxlamaq üçün : 2");
+            Console.WriteLine("Balansdan cixaris üçün : 3");
+            Console.WriteLine("--------------------------");
+        }
+
+        public static void Balance()
+        {
+            Console.Clear();
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("Sizin balansınızda {0} Azn var.", pul);
+            Console.WriteLine("--------------------------");
+        }
+
+        public static void Cashout()
+        {
+            int cash;
+            Console.Clear();
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("Min mebleg : 1 , Max mebleg : 1000");
+            Console.Write("Meblegi daxil edin : ");
+            cash = check(Console.ReadLine());
+            if (cash > 0 && cash < 1000)
+            {
+
+                int[] cashout = new int[] { 100, 50, 20, 10, 5, 1 };
+                if (pul >= cash)
+                {
+                    string date = DateTime.Now.ToString();
+                    cashdate.Add(date, cash);
+                    pul = pul - cash;
+                    for (int i = 0; i < cashout.Length; i++)
+                    {
+                        if (cash >= cashout[i]) { Console.WriteLine(cash / cashout[i] + " Eded " + cashout[i] + " Azn"); cash = cash % cashout[i]; }
+                    }
+                    Console.WriteLine("--------------------------");
+                }
+                else Console.WriteLine("Daxil etdiyiniz mebleg balansinizda yoxdur");
+
+
+            }
+            else Console.WriteLine("Daxil etdiyiniz mebleg yalnisdir");
+        }
+
+
+
+
+
+        public static void Cıxarıs()
+        {
+
+
+            Console.WriteLine("--------------------------");
+            foreach (var item in cashdate.Keys)
+            {
+                Console.WriteLine("Pulun cixarilma tarixi : {0}", item);
+                Console.WriteLine("Cixarilan pulun meblegi : {0}", cashdate[item]);
+            }
+            Console.WriteLine("--------------------------");
+
+
+        }
+
+        public static int check(string value)
+        {
+            int result = 0;
+            foreach (char item in value)
+            {
+                if (char.IsDigit(item) != true)
+                {
+                    result++;
+
+                }
+            }
+            if (result == 0) return Convert.ToInt32(value);
+            else return -1;
+
+        }
+    }
+}
+
--- week5task/Program.cs
+++ week5task/Program.cs
@@ -19,28 +19,117 @@ namespace week5task
              * Student classi yaradaraq method-lari orada yazib Program classi icerisinde Main methodunda istifade edeceksiz.
              */
 
-           
-            Console.WriteLine("-----------------------");
-            Console.Write("Ad Soyad Daxil edin : ");
-            string fullname = Console.ReadLine();
-            string[] point = new string[3];
-            string[] writevalue = new string[3] { "1-ci imtahan nəticənizi daxil edin : ", "2-ci imtahan nəticənizi daxil edin : ", "3-ci imtahan nəticənizi daxil edin : " };
-            int i = 0;
-            while (true)
-            {
-                if (i > 2) { break; }
 
-                Console.Write(writevalue[i]);
-                point[i] = Console.ReadLine();
-                if (Student.Check(point[i]) == 1) { i++; }
-                else
-                if (Student.Check(point[i]) == 2) { point[i] = "51"; i++; }
-                else { Student.Error(); }
+            //Console.WriteLine("-----------------------");
+            //Console.Write("Ad Soyad Daxil edin : ");
+            //string fullname = Console.ReadLine();
+            //string[] point = new string[3];
+            //string[] writevalue = new string[3] { "1-ci imtahan nəticənizi daxil edin : ", "2-ci imtahan nəticənizi daxil edin : ", "3-ci imtahan nəticənizi daxil edin : " };
+            //int i = 0;
+            //while (true)
+            //{
+            //    if (i > 2) { break; }
+
+            //    Console.Write(writevalue[i]);
+            //    point[i] = Console.ReadLine();
+            //    if (Student.Check(point[i]) == 1) { i++; }
+            //    else
+            //    if (Student.Check(point[i]) == 2) { point[i] = "51"; i++; }
+            //    else { Student.Error(); }
+            //}
+            //int average = Student.Average(point[0], point[1], point[2]);
+            //bool result = Student.Diplomisi(average);
+            //Student.Print(point[0], point[1], point[2], average, result, fullname); 
+
+            #endregion
+            #region 2.Para pul el cirki 2 yakinda sinemalarda
+
+            /*
+             * Evvelceden teyin edilen pin vasitesile ATM-ye yaxinlasan vetendas pini daxil edir.
+             * Qarsinina cixan menu-dan balansina baxa, negd pul cixarda, ve ya umumi hesabdan cixaris
+             * isteye biler.Secilen her hansi bir emeliyyatdan sonra istifadeci yeniden menuya- geri done bilmelidir.
+             *
+             *
+             * login
+             *
+             * pin:
+             * pin sadece reqemlerden ibaret olmalidir // 1234
+             * pin sadece 4 reqemden ibaret olmalidir.
+             *
+             *
+             * Menu
+             *
+             * 1.Balans:
+             * (evvelceden standart olaraq balans 1000 azn olaraq nezerde tutulur.)
+             *
+             * 2.Negd pulun verilmesi:
+             *  Istifadeci ATM-den min 1 manat max 1000 manat ceke biler.Balans yoxlamasi olacaq,
+             *  daxil edilen mebleg hansi esginasdan nece eded olacaq o sekilde netice olaraq gosterilir.
+             *   
+             *
+             * 3.Balansdan cixaris:
+             *   Eger balansdan mexaric olubsa mexaric olunan meblegi ve hemin tarixi cap edin,
+             *   Yox eger hele balansdan mexaric olunmayibsa bu haqqda mesaj gosterin.
+             *
+             *
+             * Qeyd: consoledan daxil edilenler yalniz eded ola biler bular yoxlayanacaq ,
+             * mumkun derece methodlarla isleyin
+             *   
+             */
+
+
+
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine("Xoş Gəlmişsiniz");
+            Console.WriteLine("Emeliyyatlar üçün");
+            Console.Write("Istifadeci kodunuzu daxil edin : ");
+
+            int pin = Atm.check(Console.ReadLine());
+            if (pin > 0)
+            {
+                bool result = Atm.Login(pin);
+
+                do
+                {
+                    if (result == true)
+                    {
+
+
+
+                        Atm.Menu();
+                        int value1;
+
+                        value1 = Convert.ToInt32(Console.ReadLine());
+                        switch (value1)
+                        {
+                            case 1:
+                                Atm.Cashout();
+                                break;
+                            case 2:
+                                Atm.Balance();
+                                break;
+                            case 3:
+                                Atm.Cıxarıs();
+                                break;
+                            default:
+                                Console.WriteLine("Duzugun deyer daxil edin");
+                                break;
+
+                        }
+
+
+                        Console.WriteLine("Emeliyyati sonlandirmaq ucun X (Enter) daxil edin");
+                        Console.WriteLine("Davam etmek ucun (Enter)");
+                    }
+                    else Console.WriteLine("Emeliyyat sonlandirildi daxil etdiyiniz kod yalnisdir"); break;
+                }
+
+                while (Console.ReadLine() != "X");
+
+
             }
-            int average = Student.Average(point[0], point[1], point[2]);
-            bool result = Student.Diplomisi(average);
-            Student.Print(point[0], point[1], point[2], average, result, fullname); 
-            
+
+            else Console.WriteLine("Emeliyyat sonlandirildi daxil etdiyiniz kod yalnisdir");
             #endregion
         }
     }
