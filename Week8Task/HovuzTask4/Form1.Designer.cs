fd742c6e9e95de76eeef10274763231c60956b6c
Elcan Seyidov <elcanseyidov1223@gmail.com>
2021-05-30 09:28:39 +0400

    Week8task4

---
+++ Week8Task/HovuzTask4/Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿
+namespace HovuzTask4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dolmavaxtiGroupBox = new System.Windows.Forms.GroupBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dolmavaxtiTextBox = new System.Windows.Forms.TextBox();
+            this.kran2TextBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.kran1TextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.hecmGroupBox = new System.Windows.Forms.GroupBox();
+            this.hecmTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.hesableButton = new System.Windows.Forms.Button();
+            this.derinlikTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.enTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.uzunluqTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dolmavaxtiGroupBox.SuspendLayout();
+            this.hecmGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dolmavaxtiGroupBox
+            // 
+            this.dolmavaxtiGroupBox.Controls.Add(this.label7);
+            this.dolmavaxtiGroupBox.Controls.Add(this.button1);
+            this.dolmavaxtiGroupBox.Controls.Add(this.dolmavaxtiTextBox);
+            this.dolmavaxtiGroupBox.Controls.Add(this.kran2TextBox);
+            this.dolmavaxtiGroupBox.Controls.Add(this.label6);
+            this.dolmavaxtiGroupBox.Controls.Add(this.kran1TextBox);
+            this.dolmavaxtiGroupBox.Controls.Add(this.label5);
+            this.dolmavaxtiGroupBox.Enabled = false;
+            this.dolmavaxtiGroupBox.Location = new System.Drawing.Point(389, 43);
+            this.dolmavaxtiGroupBox.Name = "dolmavaxtiGroupBox";
+            this.dolmavaxtiGroupBox.Size = new System.Drawing.Size(241, 340);
+            this.dolmavaxtiGroupBox.TabIndex = 3;
+            this.dolmavaxtiGroupBox.TabStop = false;
+            this.dolmavaxtiGroupBox.Text = "Dolma Vaxti";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(63, 162);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(103, 17);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Doldurma Vaxti";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(62, 251);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 37);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Hesabla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dolmavaxtiTextBox
+            // 
+            this.dolmavaxtiTextBox.Location = new System.Drawing.Point(62, 182);
+            this.dolmavaxtiTextBox.Name = "dolmavaxtiTextBox";
+            this.dolmavaxtiTextBox.ReadOnly = true;
+            this.dolmavaxtiTextBox.Size = new System.Drawing.Size(120, 22);
+            this.dolmavaxtiTextBox.TabIndex = 13;
+            // 
+            // kran2TextBox
+            // 
+            this.kran2TextBox.Location = new System.Drawing.Point(62, 111);
+            this.kran2TextBox.Name = "kran2TextBox";
+            this.kran2TextBox.Size = new System.Drawing.Size(120, 22);
+            this.kran2TextBox.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(59, 91);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(73, 17);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Ikinci Kran";
+            // 
+            // kran1TextBox
+            // 
+            this.kran1TextBox.Location = new System.Drawing.Point(62, 48);
+            this.kran1TextBox.Name = "kran1TextBox";
+            this.kran1TextBox.Size = new System.Drawing.Size(120, 22);
+            this.kran1TextBox.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(59, 28);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Brinci Kran";
+            // 
+            // hecmGroupBox
+            // 
+            this.hecmGroupBox.Controls.Add(this.hecmTextBox);
+            this.hecmGroupBox.Controls.Add(this.label4);
+            this.hecmGroupBox.Controls.Add(this.hesableButton);
+            this.hecmGroupBox.Controls.Add(this.derinlikTextBox);
+            this.hecmGroupBox.Controls.Add(this.label3);
+            this.hecmGroupBox.Controls.Add(this.enTextBox);
+            this.hecmGroupBox.Controls.Add(this.label2);
+            this.hecmGroupBox.Controls.Add(this.uzunluqTextBox);
+            this.hecmGroupBox.Controls.Add(this.label1);
+            this.hecmGroupBox.Location = new System.Drawing.Point(109, 43);
+            this.hecmGroupBox.Name = "hecmGroupBox";
+            this.hecmGroupBox.Size = new System.Drawing.Size(241, 340);
+            this.hecmGroupBox.TabIndex = 2;
+            this.hecmGroupBox.TabStop = false;
+            this.hecmGroupBox.Text = "Hovuzun hecimi";
+            // 
+            // hecmTextBox
+            // 
+            this.hecmTextBox.Location = new System.Drawing.Point(55, 251);
+            this.hecmTextBox.Name = "hecmTextBox";
+            this.hecmTextBox.ReadOnly = true;
+            this.hecmTextBox.Size = new System.Drawing.Size(120, 22);
+            this.hecmTextBox.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(52, 231);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(107, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Hovuzun Hecmi";
+            // 
+            // hesableButton
+            // 
+            this.hesableButton.Location = new System.Drawing.Point(55, 300);
+            this.hesableButton.Name = "hesableButton";
+            this.hesableButton.Size = new System.Drawing.Size(120, 34);
+            this.hesableButton.TabIndex = 6;
+            this.hesableButton.Text = "Hesabla";
+            this.hesableButton.UseVisualStyleBackColor = true;
+            this.hesableButton.Click += new System.EventHandler(this.hesableButton_Click);
+            // 
+            // derinlikTextBox
+            // 
+            this.derinlikTextBox.Location = new System.Drawing.Point(55, 182);
+            this.derinlikTextBox.Name = "derinlikTextBox";
+            this.derinlikTextBox.Size = new System.Drawing.Size(120, 22);
+            this.derinlikTextBox.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(52, 162);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(118, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Hovuzun Derinliyi";
+            // 
+            // enTextBox
+            // 
+            this.enTextBox.Location = new System.Drawing.Point(55, 111);
+            this.enTextBox.Name = "enTextBox";
+            this.enTextBox.Size = new System.Drawing.Size(120, 22);
+            this.enTextBox.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(52, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Hovuzun Eni";
+            // 
+            // uzunluqTextBox
+            // 
+            this.uzunluqTextBox.Location = new System.Drawing.Point(55, 48);
+            this.uzunluqTextBox.Name = "uzunluqTextBox";
+            this.uzunluqTextBox.Size = new System.Drawing.Size(120, 22);
+            this.uzunluqTextBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hovuzun Uzunlugu";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(757, 450);
+            this.Controls.Add(this.dolmavaxtiGroupBox);
+            this.Controls.Add(this.hecmGroupBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.dolmavaxtiGroupBox.ResumeLayout(false);
+            this.dolmavaxtiGroupBox.PerformLayout();
+            this.hecmGroupBox.ResumeLayout(false);
+            this.hecmGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox dolmavaxtiGroupBox;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox dolmavaxtiTextBox;
+        private System.Windows.Forms.TextBox kran2TextBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox kran1TextBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox hecmGroupBox;
+        private System.Windows.Forms.TextBox hecmTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button hesableButton;
+        private System.Windows.Forms.TextBox derinlikTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox enTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox uzunluqTextBox;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Week8Task/HovuzTask4/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HovuzTask4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void hesableButton_Click(object sender, EventArgs e)
+        {
+
+            hecmTextBox.Text = Hecm(Convert.ToInt32(uzunluqTextBox.Text), Convert.ToInt32(enTextBox.Text), Convert.ToInt32(derinlikTextBox.Text)).ToString();
+            dolmavaxtiGroupBox.Enabled = true;
+        }
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double sum = Convert.ToDouble(kran1TextBox.Text) + Convert.ToInt32(kran2TextBox.Text);
+            double hecm = Convert.ToDouble(hecmTextBox.Text);
+            double dolmavaxti = hecm / sum;
+            dolmavaxtiTextBox.Text = dolmavaxti.ToString();
+        }
+
+        public int Hecm(int hundurluk, int en, int uzunluq)
+        {
+            return hundurluk * uzunluq * en;
+        }
+    }
+}
